cca9dd31a793530af462ecac38746ad09888061d
ilan cabrita <ilan.cbrita.info@gmail.com>
2021-11-05 11:53:54 -0300

    maj JS 1 05.11.21

--- front/JavaScript/index.js
+++ front/JavaScript/index.js
@@ -3,10 +3,23 @@ let val = find();
 let val = findById();
 let val = product()
 
-function () 
+function find()
 {
   fetch('http://localhost:3000/api/products')
     .then(response => response.json())
     .then(data => console.log(JSON.stringify(data)))
 }
 
+function findById()
+{
+  fetch('http://localhost:3000/api/products')
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)))
+}
+
+function product()
+{
+  fetch('http://localhost:3000/api/products')
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)))
+}
