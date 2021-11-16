5374c2b59a59d405655d4facb356e33e51845b39
ilan cabrita <ilan.cbrita.info@gmail.com>
2021-11-16 17:38:10 +0100

    maj JS 1 16.11.21

---
+++ front/JavaScript/confirmation.js
@@ -0,0 +1,25 @@
+import { find , findById , product } from 'Product'
+let val = find();
+let val = findById();
+let val = product()
+
+function find()
+{
+  fetch('http://localhost:3000/api/products')
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)))
+}
+
+function findById()
+{
+  fetch('http://localhost:3000/api/products')
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)))
+}
+
+function product()
+{
+  fetch('http://localhost:3000/api/products')
+    .then(response => response.json())
+    .then(data => console.log(JSON.stringify(data)))
+}
