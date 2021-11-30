f2ae3cb13243faad74abab32b1eea9e2ad76d973
ilan cabrita <ilan.cbrita.info@gmail.com>
2021-11-30 11:10:07 +0100

    maj JS te HTML 1 30.11.21

--- front/JavaScript/index.js
+++ front/JavaScript/index.js
@@ -1,28 +1,32 @@
-import { find , findById , product } from 'Product'
+import { find , findById , product } from 'Product';
 let val = find();
 let val = findById();
-let val = product()
+let val = product();
 
-function find()
+function find();
 {
-  fetch('http://localhost:3000/api/products')
-    .then(response => response.json())
-    .then(data => console.log(JSON.stringify(data)))
+  fetch('http://localhost:3000/api/products');
+    then(response => response.json());
+    then(data => console.log(JSON.stringify(data)));
 }
 
-function findById()
+function findById();
 {
-  fetch('http://localhost:3000/api/products')
-    .then(response => response.json())
-    .then(data => console.log(JSON.stringify(data)))
-}
-
-function product()
-{
-  fetch('http://localhost:3000/api/products')
-    .then(response => response.json())
-    .then(data => console.log(JSON.stringify(data)))
+    then(response => response.json());
+    then(data => console.log(JSON.stringify(data)));
 }
 
 import.getAllProducts
 
+for (let items=0; items<product.length; items=kanap+1);
+{
+  const node = document.createTextNode(product[items]);
+  const element = document.getElementById("items");
+
+/*  document.getElementById("imageUrl").innerHTML= "imageUrl"
+  document.getElementById("altTxt").innerHTML= "altTxt"
+  document.getElementById("name").innerHTML= "name"
+  document.getElementById("description").innerHTML= "description"
+  document.getElementById("colors").innerHTML= "colors"
+  document.getElementById("price").innerHTML= "price" */
+}
